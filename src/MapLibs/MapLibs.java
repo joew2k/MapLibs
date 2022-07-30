26e869ce0a7e10d74ea06d2d25104bb04e3b41d6
joew2k <ogwuchejoseph25@gmail.com>
2022-07-30 23:35:08 +0100

    method working properly

---
+++ src/MapLibs/MapLibs.java
@@ -0,0 +1,27 @@
+package MapLibs;
+
+import java.util.Scanner;
+
+public class MapLibs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Get user input for season of the year.
+		System.out.println("Enter season of the year");
+		Scanner scanner = new Scanner(System.in);
+		String season = scanner.next();
+		
+		// Ask user to enter a whole number
+		System.out.println("Enter a whole number");
+		int wholeNumber = scanner.nextInt();
+		
+		// Ask user to enter an adjective
+		System.out.println("Enter an adjective");
+		String adjective = scanner.next();
+		scanner.close();
+		// output "On a [adjective][season of the year] day, i drink a minimum of [whole number] cups of coffee"
+		System.out.println("On a " + adjective + " "+ season + " day, i drink a minimum of " + wholeNumber + " cups of coffee");
+
+	}
+
+}
